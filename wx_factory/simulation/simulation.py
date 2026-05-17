8cfa79bf4d94c841a7e18c3c441add84994207ff
Martin <ma3080hi-s@student.lu.se>
2026-05-17 23:41:02 +0200

    Working?

--- wx_factory/simulation/simulation.py
+++ wx_factory/simulation/simulation.py
@@ -208,6 +208,7 @@ class Simulation:
             entropy_func = entropy_function(self.Q,self.geometry)
             # TODO: move integration to the function or use the one from RHSDirectFluxReconstruction_ESAV
             entropy_func_integrated = self.geometry.Δx1 / 2.0 * self.geometry.Δx3 / 2.0 * xp.sum(entropy_func * self.operators.weights_volume_integral)
+
             
             self.output.step(self.Q, self.step_id, entropy_func_integrated, self.rhs.full.epsilon)  # Perform any requested output
             sys.stdout.flush()
